0992b59b5b18a484042f284890b08b90aea0b0dc
guerraj97 <39325349+guerraj97@users.noreply.github.com>
2020-02-02 11:44:26 -0600

    Update PSO_algorithm.m
    3era version del PSO

--- PSO_algorithm.m
+++ PSO_algorithm.m
@@ -1,4 +1,4 @@
-function PSO_algotith(dim,n_swarm, c1, c2, w,n_iteration, max_run,obj_fun,wdamp)
+function PSO_algotithm(dim,n_swarm, c1, c2, w,n_iteration, max_run,obj_fun,wdamp)
 run = 0;
 
 pbest = zeros(n_swarm,dim);
@@ -10,20 +10,41 @@ X = 100*rand(n_swarm,dim);
 % Generate Random Solution for Particle Position
 V = 100*rand(n_swarm,dim);
 
-pbest = X;
+% for ni=1:n_swarm
+% f_pbest(ni,:)=obj_fun(X(ni,:));
+% end
+% [fgbest,igbest]=min(f_pbest);
+% gbest=X(igbest,:)
+% pbest=X;
+% fpbest=f_pbest;
 
+
+while run < max_run
 for i = 1:n_swarm
-    fpbest(i,:) = obj_fun(X(i,:));
+    if (f_pbest < pbest)
+    pbest = X;
+    end
+    fpbest(i,:) = min(obj_fun(X(i,:)));
+end
+
+%Actualizar ecuaciones
+for particle = 1:n_swarm
+for dim = 1:dim
+
+%velocidad
+V(particle,:) = w*V(particle,:)+ c1*rand(1,dim).*(pbest(particle,:) - X(particle,:)) ...
+              + c2*rand(1,dim).*(gbest(particle,:) - X(particle,:));
+%posicion
+ X(particle,:) = X(particle,:) + V(particle,:);
+end
+
 end
 
 
-% % Update Velocity
-% V (swarm_particle,:) = w*V(swarm_particle,:) ...
-% + c1*rand(1,dim).*(pbest_position(swarm_particle,:) - X(swarm_particle,:)) ...
-%               + c2*rand(1,dim).*(gbest - X(swarm_particle,:));
+
 %
-% %actualizar posicion
-% X(swarm_particle,:) = X(swarm_particle,:) + V(swarm_particle,:);
+%actualizar posicion
+
 %             costo(i,:) = myfun(X(i,:));
 %             % Update Personal Best
 %             if costo(i) < pbest_cost(i)
@@ -34,7 +55,10 @@ end
 %                 gbest = pbest_position(I,:);
 %         bestcost(it) = M;
 %
-% run = run + 1;
+ run = run + 1
+pbest
+gbest
 %         w = w * wdamp;
-
+end
+f_pbest;
 end
