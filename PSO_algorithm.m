b76d9cc78786af1b1fd9e37de5bda95efd08f5ec
guerraj97 <39325349+guerraj97@users.noreply.github.com>
2020-02-02 10:33:24 -0600

    Update PSO_algorithm.m
    2da. version del PSO

--- PSO_algorithm.m
+++ PSO_algorithm.m
@@ -1,33 +1,40 @@
-function PSO_algotith(dim,n_swarm, c1, c2, W,n_iteration, obj_fun, max_run)
-clear;
-clc;
-close all;
-V = zero(1,dim);
+function PSO_algotith(dim,n_swarm, c1, c2, w,n_iteration, max_run,obj_fun,wdamp)
+run = 0;
+
+pbest = zeros(n_swarm,dim);
+gbest = 1000000*ones(n_swarm,dim);
+f_pbest = zeros(n_swarm,dim);
+
+% Generate Random Solution for Particle Position
+X = 100*rand(n_swarm,dim);
+% Generate Random Solution for Particle Position
+V = 100*rand(n_swarm,dim);
+
+pbest = X;
 
 for i = 1:n_swarm
-    for dimension = 1:dim
-        %inicializa al grupo
-        %inicializa la velocidad
-    end
+    fpbest(i,:) = obj_fun(X(i,:));
 end
 
-for iteration = 1:n_iteration
-    for swarm_particule = 1:n_swarm
-        %calcular fitness
-        if %pbest_actual es mejor al pasado
-            %pbest = pbest_actual
-        end
-    end
 
+% % Update Velocity
+% V (swarm_particle,:) = w*V(swarm_particle,:) ...
+% + c1*rand(1,dim).*(pbest_position(swarm_particle,:) - X(swarm_particle,:)) ...
+%               + c2*rand(1,dim).*(gbest - X(swarm_particle,:));
+%
+% %actualizar posicion
+% X(swarm_particle,:) = X(swarm_particle,:) + V(swarm_particle,:);
+%             costo(i,:) = myfun(X(i,:));
+%             % Update Personal Best
+%             if costo(i) < pbest_cost(i)
+%                 pbest_position(i,:) = X(i,:);
+%                 pbest_cost(i) = costo(i);
+%             % Update Global Best
+%                 [M,I] = min(pbest_cost);
+%                 gbest = pbest_position(I,:);
+%         bestcost(it) = M;
+%
+% run = run + 1;
+%         w = w * wdamp;
 
-%ahora tomando el mejor valor global
-for pswarm = 1:n_swarm
-    for dim2 = 1:dim
-        %calcular velocidad como
-        %V(iteration+1) = W*V(iteration) + c1 rand(P - X) + c2 rand(Pg - X)
-
-        %actualizar posicion
-        %X(iteration+1) = X(iteration) + V(iteration+1)
-    end
-end
 end
