999f4d8c65f7967a0a04a1cf50613efcea25fa49
guerraj97 <39325349+guerraj97@users.noreply.github.com>
2020-02-07 20:08:28 -0600

    Update PSO.m
    Actualizacion del script principal PSO.m

--- PSO.m
+++ PSO.m
@@ -3,18 +3,21 @@
 %Utiliza la funcion PSO_algotith.
 %% Funcion a utilizar y deficion de parametros.
 
-obj_fun = @(x); %definicion de la funcion objetivo que se quiere optimizar
-Vars = ; %numero de variables o numero de incognitas.
+obj_fun = @(x) sum(x.^2); %definicion de la funcion objetivo que se quiere optimizar
+Vars = 3; %numero de variables o numero de incognitas.
 varSolution = [1 Vars]; %Matriz de soluciones.
 
-Lb = 0;
-Ub = 0;
+Lb = -10;
+Ub = 10;
 
-nIteration = ; %maxima numero de iteraciones
+nIteration = 30; %maxima numero de iteraciones
 
-n_swarm = ;%numero de agentes, poblacion
+n_swarm = 4;%numero de agentes, poblacion
 
-w = ; %coeficiente de inercia
+w = 1; %coeficiente de inercia
 
-c1 = ; %coeficiente de aceleracion (personal)
-c2 = ; %coeficiente de aceleracion (grupal)
+c1 = 2; %coeficiente de aceleracion (personal)
+c2 = 2; %coeficiente de aceleracion (grupal)
+
+
+[a,gbest] = PSO_algotithm(varSolution,n_swarm,obj_fun,Lb,Ub);
