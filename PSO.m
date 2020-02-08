7007d05408d5ba7bb1c6cbc5af54cb4b58cfa600
guerraj97 <39325349+guerraj97@users.noreply.github.com>
2020-02-07 18:48:35 -0600

    Update PSO.m
    Primer borrador del script

---
+++ PSO.m
@@ -0,0 +1,20 @@
+%%
+%Jose Pablo Guerra - Implementacion del Algoritmo PSO.
+%Utiliza la funcion PSO_algotith.
+%% Funcion a utilizar y deficion de parametros.
+
+obj_fun = @(x); %definicion de la funcion objetivo que se quiere optimizar
+Vars = ; %numero de variables o numero de incognitas.
+varSolution = [1 Vars]; %Matriz de soluciones.
+
+Lb = 0;
+Ub = 0;
+
+nIteration = ; %maxima numero de iteraciones
+
+n_swarm = ;%numero de agentes, poblacion
+
+w = ; %coeficiente de inercia
+
+c1 = ; %coeficiente de aceleracion (personal)
+c2 = ; %coeficiente de aceleracion (grupal)
