93b9d4ee93597f36a7a652d9e15bb5d618864ac2
guerraj97 <39325349+guerraj97@users.noreply.github.com>
2020-02-17 17:20:03 -0600

    Update PSO.m
    Vesion preeliminar del main para la funcion

--- PSO.m
+++ PSO.m
@@ -1,23 +1,39 @@
-%%
-%Jose Pablo Guerra - Implementacion del Algoritmo PSO.
-%Utiliza la funcion PSO_algotith.
-%% Funcion a utilizar y deficion de parametros.
+%% Jose Pablo Guerra - Implementacion del Algoritmo PSO.
+%  Utiliza la funcion PSO_algotith.
+%% Funcion a utilizar y definicion de parametros.
 
 obj_fun = @(x) sum(x.^2); %definicion de la funcion objetivo que se quiere optimizar
-Vars = 3; %numero de variables o numero de incognitas.
+Vars = 2; %numero de variables o numero de incognitas.
 varSolution = [1 Vars]; %Matriz de soluciones.
-
+min_function = min(obj_fun);
 Lb = -10;
 Ub = 10;
 
-nIteration = 30; %maxima numero de iteraciones
+nIteration = 200; %maxima numero de iteraciones
 
-n_swarm = 4;%numero de agentes, poblacion
-
+n_swarm = 100;%numero de agentes, poblacion
 w = 1; %coeficiente de inercia
-
+wdamp = 0.99;
 c1 = 2; %coeficiente de aceleracion (personal)
 c2 = 2; %coeficiente de aceleracion (grupal)
+init_particle = zeros(n_swarm,2);
+n = 1;
+[a,gbest,status,particle,iteration_over] = PSO_algorithm(varSolution,n_swarm,obj_fun,Lb,Ub,nIteration,w,c1,c2,wdamp);
+
+for pop = 1:n_swarm
+          init_particle(pop,1) = particle(pop).pos(1);
+          init_particle(pop,2) = particle(pop).pos(2);
+end
+
+figure(1);clf;
+plot(init_particle(:,1),init_particle(:,2),'ro');
+drawnow;
+
+    figure(2);clf;
+    plot(status);
+
+    iteration_over
+%la region p es mas inmune a ruido que la region n porque?
 
 
-[a,gbest] = PSO_algotithm(varSolution,n_swarm,obj_fun,Lb,Ub);
+%60
