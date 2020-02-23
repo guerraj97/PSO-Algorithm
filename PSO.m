585fcb5cfd6b966bec673e44d34623f45a0dd044
guerraj97 <39325349+guerraj97@users.noreply.github.com>
2020-02-23 17:54:51 -0600

    Update PSO.m
    Version final de la funcion principal PSO

--- PSO.m
+++ PSO.m
@@ -2,14 +2,21 @@
 %  Utiliza la funcion PSO_algotith.
 %% Funcion a utilizar y definicion de parametros.
 
-obj_fun = @(x) sum(x.^2); %definicion de la funcion objetivo que se quiere optimizar
+%paraboloide 0
+%Shubert -186.7309
+%Bohachevsky 0
+%Goldstein 3
+%Powell 0
+%eggholder -959.6407
+
+%obj_fun = @(x) sin(10*pi.*x) / (2.*x) + (x-1).^4; %definicion de la funcion objetivo que se quiere optimizar
 Vars = 2; %numero de variables o numero de incognitas.
 varSolution = [1 Vars]; %Matriz de soluciones.
 %min_function = min(obj_fun);
 Lb = -10;
 Ub = 10;
 
-nIteration = 200; %maxima numero de iteraciones
+BigLoop = 100; %maxima numero de iteraciones
 
 n_swarm = 100;%numero de agentes, poblacion
 w = 1; %coeficiente de inercia
@@ -19,7 +26,7 @@ c2 = 2; %coeficiente de aceleracion (grupal)
 init_particle = zeros(n_swarm,2);
 n = 1;
 clf;
-[a,gbest,status,particle,iteration_over] = PSO_algorithm(varSolution,n_swarm,obj_fun,Lb,Ub,nIteration,w,c1,c2,wdamp);
+[a,gbest,status,particle,iteration_over] = PSO_algorithm(varSolution,n_swarm,Lb,Ub,BigLoop,w,c1,c2,wdamp);
 
 for pop = 1:n_swarm
           init_particle(pop,1) = particle(pop).pos(1);
