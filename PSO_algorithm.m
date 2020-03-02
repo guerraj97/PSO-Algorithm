47585822611ff7e24595ef6ef1255d2ac100a155
guerraj97 <39325349+guerraj97@users.noreply.github.com>
2020-03-01 20:50:28 -0600

    Update PSO_algorithm.m
    El algoritmo ya esta al 100%
    Cuenta con 3 criterios de convergencia: Numero de iteraciones, que cierto porcentaje de particulas este dentro del rango establecido (radio de convergencia) y que si no hay cambio significativo entre los costos globales, de igual forma lo detiene.

--- PSO_algorithm.m
+++ PSO_algorithm.m
@@ -22,7 +22,8 @@
 % initParticle =
 
 %%
-function [swarm_p,gbest,Status_by_cost,initParticle,iteration_over] = PSO_algorithm(n_varsize,n_swarm,Lb,Ub,Loop,w,c1,c2,damp)
+function [swarm_p,gbest,Status_by_cost,initParticle,iteration_over] = PSO_algorithm(n_varsize,...
+                                                n_swarm,max_convergence,Lb,Ub,w,c1,c2,damp)
 %run = 0;
 
 %inicializacion de parametros.
@@ -35,19 +36,23 @@ initParticle = [];
 part.b.Pos = [];
 part.b.Costo = [];
 
-iteration_over = zeros(Loop,3);
+iteration_over = zeros(100,2);
 
-maxIteration = 200;
+CostoParticulas = zeros(200,1);
+option = 5;
+N = 1; %control de iteraciones para estadistica
+%% aqui cambio de codigo para poder probar variacion de parametros.
+
+%for iteration_Var = 1:100
+
+count_iteration = 1;
+
 %genero cada particula del swarm.
 swarm_p = repmat(part, n_swarm, 1);
 initParticle = repmat(part, n_swarm, 1);
 %vector para el ploteo
 plot_swarm = zeros(n_swarm,2);
 
-
-for count = 1:Loop
-stat = 1;
-var = w;
 %global best al inicio
 gbest.Costo = inf; %al ser un problema de minimazion se toma el infinito.
 for i = 1:n_swarm
@@ -57,7 +62,7 @@ for i = 1:n_swarm
     swarm_p(i).velocity = zeros(n_varsize);
 
     %evaluando la primera iteracion
-    swarm_p(i).costo = obj_fun(swarm_p(i).pos);
+    swarm_p(i).costo = obj_fun(swarm_p(i).pos,option);
 
     %Buscando una funcion de costo inicial e inicializando la mejor
     %posicion
@@ -68,14 +73,17 @@ for i = 1:n_swarm
     end
 end
 
-Status_by_cost = zeros(maxIteration,1);
+%Status_by_cost = zeros(200,1);
 
-% figure(3);
-% clf;
-for iteration = 1:maxIteration
-%     plot(plot_swarm(:,1),plot_swarm(:,2),'ro')
-%     axis([Lb Ub Lb Ub])
-%     pause(0.1);
+figure(3);
+clf;
+for iteration = 1:200
+    plot(plot_swarm(:,1),plot_swarm(:,2),'ro');
+    axis([Lb Ub Lb Ub]);
+    title('Movimiento de las partículas');
+    grid on;
+    pause(0.1);
+
     for pop = 1:n_swarm
 
         %velocidad
@@ -87,7 +95,7 @@ for iteration = 1:maxIteration
          swarm_p(pop).pos = swarm_p(pop).pos + swarm_p(pop).velocity;
 
          %evaluando la funcion de costo
-         swarm_p(pop).costo = obj_fun(swarm_p(pop).pos);
+         swarm_p(pop).costo = obj_fun(swarm_p(pop).pos,option);
 
          if swarm_p(pop).costo < swarm_p(pop).b.Costo
              swarm_p(pop).b.Pos = swarm_p(pop).pos;
@@ -99,32 +107,41 @@ for iteration = 1:maxIteration
          end
          plot_swarm(pop,1) = swarm_p(pop).pos(1);
          plot_swarm(pop,2) = swarm_p(pop).pos(2);
+         CostoParticulas(pop) = swarm_p(pop).costo;
     end
+    %CostoParticulas = gbest.Costo;
 
     Status_by_cost(iteration) = gbest.Costo;
-    if Status_by_cost(iteration) < -959.6407
-            iteration_over(count,1) = iteration;
-            iteration_over(count,2) = Status_by_cost(iteration);
-            iteration_over(count,3) = var;
-            stat = 0;
-            break;
-    end
-    %disp(['iteration ' num2str(iteration) ' BesCosto = ' num2str(Status_by_cost(iteration))])
-end
-disp(['iteration ' num2str(count) ])
+    %M = min(Status_by_cost);
+    %w = w*damp;
 
-if stat == 1
-M = min(Status_by_cost);
-for i = 1:maxIteration
-        if Status_by_cost(i) == M
-            iteration_over(count,1) = i;
-            iteration_over(count,2) = M;
-            iteration_over(count,3) = var;
+        if Status_by_cost(iteration) < max_convergence
+        if sum(CostoParticulas(:)) < (max_convergence * n_swarm)
+            iteration_over(N,1) = iteration;
+            iteration_over(N,2) = Status_by_cost(iteration);
+            N = N + 1;
             break;
         end
-end
-end
-w = w*damp;
+        elseif iteration == 1
+            %no hago nada
+        elseif Status_by_cost(iteration) ~= Status_by_cost(iteration - 1)
+              count_iteration = 1;
+        elseif Status_by_cost(iteration) == Status_by_cost(iteration - 1)
+              count_iteration = count_iteration + 1;
+
+            if count_iteration > 10
+            %count_iteration = 1;
+            iteration_over(N,1) = iteration;
+            iteration_over(N,2) = Status_by_cost(iteration);
+            %N = N + 1;
+            break;
+            end
+        end
+
+        disp(['iteration ' num2str(iteration) ' BesCosto = ' num2str(Status_by_cost(iteration))])
 
 end
+ %w =w*damp;
+ %disp(['iteration ' num2str(iteration_Var)])
+
 end
